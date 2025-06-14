93a06e49d5449d91202a755721b45e890c7d0956
Muhammad Saad <148999719+Zabdin19@users.noreply.github.com>
2025-06-14 13:09:15 +0500

    Initial commit

---
+++ airplane_mode/doctype/airport/test_airport.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Zain (Xeverse Planet) and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirport(FrappeTestCase):
+	pass
